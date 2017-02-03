de9cdcf0e3e91da315d5c29af810e258b5da38a4
Jens Müller <jensmuller@Jenss-MBP.fritz.box>
2017-02-03 20:39:47 +0100

    First little wokrflow

--- MyWayOfLife/app/src/main/java/com/example/jensmuller/mywayoflife/OverviewMap.java
+++ MyWayOfLife/app/src/main/java/com/example/jensmuller/mywayoflife/OverviewMap.java
@@ -1,18 +1,19 @@
 package com.example.jensmuller.mywayoflife;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class OverviewMap extends FragmentActivity implements OnMapReadyCallback {
+public class OverviewMap extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Button menuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +38,19 @@ public class OverviewMap extends FragmentActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setTrafficEnabled(true);
+        menuButton = (Button) findViewById(R.id.menubutton);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = null;
+                try {
+                    intent = new Intent(createPackageContext("com.example.jensmuller.mywayoflife", 0) ,TabedActivity.class);
+                    startActivity(intent);
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
